7b187815d64593c7460e4fedc50517834be97e7c
arijitedu <85024832+arijitedu@users.noreply.github.com>
2021-05-29 20:27:27 +0530

    First Submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { LanderComponent } from "./lander/lander.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LanderComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LanderComponent]
 })
 export class AppModule {}
---
+++ src/app/lander/lander.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-lander",
+  templateUrl: "./lander.component.html"
+})
+export class LanderComponent {}
